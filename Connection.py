5bee6e65920d39b4c080dbca336c8466ec055162
Berg <vandenberg.jutai@gmail.com>
2017-01-31 00:36:33 -0400

    Populando ok

--- Connection.py
+++ Connection.py
@@ -1,7 +1,14 @@
 # coding=utf-8
 import psycopg2
+import random
 
 class Connection():
+    lstCity = None
+    lstCasaShow = None
+    lstRestaurante = None
+    lstHotel = None
+    lstMuseu = None
+    lstFundador = None
 
     def __init__(self):
         self.con = psycopg2.connect(host='localhost', port='5432', database='TURISMO_DB', user='postgres', password='041097')
@@ -16,65 +23,140 @@ class Connection():
 
     def select_data_in_city(self):
         self.cur.execute('SELECT codigo FROM cidade')
-        rs = self.cur.fetchall()
-        print rs
+        codigos = [data[0] for data in self.cur.fetchall()]
+        return codigos
 
     def insert_data_in_casa_show(self):
         file = open('./Data/casa_show.txt', 'r')
+        if (self.lstCity == None): self.lstCity = self.select_data_in_city()
+        if (self.lstRestaurante == None): self.lstRestaurante = self.select_data_in_restaurante()
+
+        j = 0
         for i in file:
-            horario_inicio, dia_fecha, descricao, nome, endereco = i.split('|')
-            self.cur.execute("INSERT INTO casa_show VALUES (%s, '%s', '%s', '%s', '%s', %d)" % ('default', dia_fecha,
-                                                                                                descricao, nome, endereco,
-                                                                                                int(horario_inicio)))
+            nome, rua, bairro, numero, descricao, horario_inicio, dia_fecha = i.split('|')
+            if j % 2 == 0:
+                self.cur.execute("INSERT INTO casa_show VALUES "
+                                 "(%s, '%s', '%s', '%s', '%s', '%s', %d, '%s', %d, %d)" % ('default', nome, rua, bairro, numero,
+                                  descricao,  int(horario_inicio), dia_fecha, random.choice(self.lstCity), random.choice(self.lstRestaurante)))
+            else:
+                self.cur.execute("INSERT INTO casa_show VALUES "
+                                 "(%s, '%s', '%s', '%s', '%s', '%s', %d, '%s', %d)" % ('default', nome, rua, bairro, numero,
+                                  descricao, int(horario_inicio), dia_fecha, random.choice(self.lstCity)))
+
+            j = j+1
         self.con.commit()
+
+    def select_data_in_casa_show(self):
+        self.cur.execute('SELECT codigo FROM casa_show')
+        codigos = [data[0] for data in self.cur.fetchall()]
+        return codigos
 
     def insert_data_in_fundador(self):
         file = open('./Data/fundadores.txt', 'r')
         for i in file:
             data_nascimento, data_morte, nacionalidade, profissao, nome = i.split('|')
-            self.cur.execute("INSERT INTO fundador VALUES (%s, '%s', '%s', '%s', '%s', %s)" % ('default', data_nascimento, data_morte,
+            self.cur.execute("INSERT INTO fundador VALUES (%s, '%s', '%s', '%s', '%s', '%s')" % ('default', data_nascimento, data_morte,
                                                                                       nacionalidade, profissao, nome))
         self.con.commit()
 
+    def select_data_in_fundador(self):
+        self.cur.execute('SELECT codigo FROM fundador')
+        codigos = [data[0] for data in self.cur.fetchall()]
+        return codigos
+
     def insert_data_in_hotel(self):
         file = open('./Data/hoteis.txt', 'r')
+        if (self.lstCity == None): self.lstCity = self.select_data_in_city()
+        if (self.lstRestaurante == None): self.lstRestaurante = self.select_data_in_restaurante()
+
+        j = 0
         for i in file:
-            tipo, nome, endereco = i.split('|')
-            self.cur.execute("INSERT INTO hotel VALUES (%s, '%s', '%s', '%s')" % ('default', tipo, nome, endereco))
+            nome, rua, bairro, numero, tipo = i.split('|')
+            if j % 2 == 0:
+                self.cur.execute("INSERT INTO hotel VALUES "
+                                 "(%s, '%s', '%s', '%s', '%s', %d, %d, %d)" % ('default', nome, rua, bairro, numero,
+                                  random.choice(self.lstCity), int(tipo), random.choice(self.lstRestaurante)))
+            else:
+                self.cur.execute("INSERT INTO hotel VALUES "
+                                 "(%s, '%s', '%s', '%s', '%s', %d, %d)" % ('default', nome, rua, bairro, numero,
+                                  random.choice(self.lstCity), int(tipo)))
+
+            j = j+1
         self.con.commit()
+
+    def select_data_in_hotel(self):
+        self.cur.execute('SELECT codigo FROM hotel')
+        codigos = [data[0] for data in self.cur.fetchall()]
+        return codigos
 
     def insert_data_in_igreja(self):
         file = open('./Data/igrejas.txt', 'r')
+        if (self.lstCity == None): self.lstCity = self.select_data_in_city()
+
         for i in file:
-            estilo, data_construcao, descricao, nome, endereco = i.split('|')
-            self.cur.execute("INSERT INTO igreja VALUES (%s, '%s', '%s', '%s', '%s', '%s')" % ('default', estilo,
-                                                                                                data_construcao, descricao,
-                                                                                                nome, endereco))
+            nome, rua, bairro, numero, descricao, estilo, data_construcao = i.split('|')
+            self.cur.execute("INSERT INTO igreja VALUES (%s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', %d)" % ('default', nome, rua, bairro,
+                             numero, descricao, estilo, data_construcao, random.choice(self.lstCity)))
         self.con.commit()
 
     def insert_data_in_museu(self):
         file = open('./Data/museus.txt', 'r')
+        if (self.lstCity == None): self.lstCity = self.select_data_in_city()
         for i in file:
-            n_salas, descricao, data_fundacao, endereco, nome = i.split('|')
-            self.cur.execute("INSERT INTO museu VALUES (%s, '%s', '%s', '%s', '%s', %d)" % ('default', descricao, data_fundacao,
-                                                                                            endereco, nome, int(n_salas)))
+            nome, rua, bairro, numero, descricao, n_salas = i.split('|')
+            self.cur.execute("INSERT INTO museu VALUES (%s, '%s', '%s', '%s', '%s', '%s', %d, %d)" % ('default', nome, rua, bairro,
+                                                                                            numero, descricao, int(n_salas), random.choice(self.lstCity)))
         self.con.commit()
 
+    def select_data_in_museu(self):
+        self.cur.execute('SELECT codigo FROM museu')
+        codigos = [data[0] for data in self.cur.fetchall()]
+        return codigos
+
     def insert_data_in_restaurante(self):
-        file = open('./Data/museus.txt', 'r')
+        file = open('./Data/restaurantes.txt', 'r')
+        if (self.lstCity == None): self.lstCity = self.select_data_in_city()
+
         for i in file:
-            especialidade, preco_medio, tipo, nome, endereco = i.split('|')
-            self.cur.execute("INSERT INTO restaurante VALUES (%s, %s, '%d', '%s', '%s', '%s')" % ('default', especialidade,
-                                                                                                preco_medio, tipo,
-                                                                                                nome, endereco))
+            nome, rua, bairro, numero, especialidade, preco_medio, tipo = i.split('|')
+            self.cur.execute("INSERT INTO restaurante VALUES (%s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', %d)" % ('default', nome, rua, bairro,
+                                                                                                  numero, especialidade,
+                                                                                                  preco_medio, tipo,
+                                                                                                  random.choice(self.lstCity)))
         self.con.commit()
+
+    def select_data_in_restaurante(self):
+        self.cur.execute('SELECT codigo FROM restaurante')
+        codigos = [data[0] for data in self.cur.fetchall()]
+        return codigos
 
     def insert_data_in_quarto(self):
         file = open('./Data/quartos.txt', 'r')
+        if (self.lstHotel == None): self.lstHotel = self.select_data_in_hotel()
+
         for i in file:
             preco, tipo = i.split('|')
-            self.cur.execute("INSERT INTO quarto VALUES (%s, %d, '%s', '%s', '%s', '%s')" % ('default', preco, tipo))
+            self.cur.execute("INSERT INTO quarto VALUES (%d, '%s', %d, %s)" % (float(preco), tipo, random.choice(self.lstHotel), 'default'))
+        self.con.commit()
+
+    def insert_data_in_fundacoes(self):
+        file = open('./Data/fundacoes.txt', 'r')
+        if (self.lstMuseu == None): self.lstMuseu = self.select_data_in_museu()
+        if (self.lstFundador == None): self.lstFundador = self.select_data_in_fundador()
+
+        for i in file:
+            data = str(i)
+            self.cur.execute("INSERT INTO funda VALUES (%d, %d, '%s')" % (random.choice(self.lstFundador),
+                                                                              random.choice(self.lstMuseu), data))
         self.con.commit()
 
 c = Connection()
-c.select_data_in_city()
+# c.insert_data_in_city()
+# c.insert_data_in_restaurante()
+# c.insert_data_in_casa_show()
+# c.insert_data_in_hotel()
+#c.insert_data_in_quarto()
+#c.insert_data_in_igreja()
+#c.insert_data_in_museu()
+#c.insert_data_in_fundador()
+#c.insert_data_in_fundacoes()
